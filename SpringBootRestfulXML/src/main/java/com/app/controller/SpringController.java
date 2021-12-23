4c80a191b241c73f3cd0772456fcf15da6323712
k <k@DESKTOP-A3TO8VT>
2021-12-24 01:43:19 +0530

    [SpringBootRestful]First Commit

---
+++ SpringBootRestfulXML/src/main/java/com/app/controller/SpringController.java
@@ -0,0 +1,27 @@
+
+package com.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model.Customer;
+
+@RestController
+public class SpringController {
+
+	@GetMapping(path="/get-cust-info")
+	public Customer customerInformation() 
+	{
+		Customer cust = new Customer();
+		
+		cust.setCustNo(2011);
+		cust.setName("Jerome");
+		cust.setCountry("United States");
+		
+		return cust;
+	}
+}
+
+
+// URL: http://localhost:8080/springbootrestxml/get-cust-info
+// URL: http://localhost:8080/springbootrestxml/get-cust-info.json
